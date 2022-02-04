3ad1a068477ad95d924a2019f4908073507c18b7
Tephen <rainbowar98@gmail.com>
2022-02-04 16:17:59 +0800

    fix pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,7 +5,7 @@ int main(){
     int p2c[2], c2p[2];//两组管道分别用于两个方向通信
     pipe(p2c);pipe(c2p);
     char a;
-    if(fork() == 0){//子进程
+    if(fork() == 0){//子进程先读后写
         close(p2c[1]);
         read(p2c[0], &a, 1);
         close(p2c[0]);
@@ -13,7 +13,7 @@ int main(){
         close(c2p[0]);
         write(c2p[1], "p", 1);
         close(c2p[1]);
-    }else {
+    }else {//父进程先写后读
         close(p2c[0]);
         write(p2c[1], "c", 1);
         close(p2c[1]);
